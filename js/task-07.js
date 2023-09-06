261ad4087848540741ca2d1a13097aa09d2d5f0b
artem-sedliar <artemsedlar@gmail.com>
2023-09-06 13:36:03 +0300

    Summary

--- js/task-07.js
+++
@@ -1,6 +0,0 @@
-const inputChange = document.querySelector('input#font-size-control')
-const abracadabra = document.querySelector('span#text')
-
-inputChange.addEventListener('input', e => {
-    abracadabra.style.fontSize = `${e.target.value }px`
-})
