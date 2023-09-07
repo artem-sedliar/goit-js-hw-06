d6bbd27db6a3712cb899c23ee1897c56b972494d
artem-sedliar <artemsedlar@gmail.com>
2023-09-07 13:58:52 +0300

    Update task 02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatoes",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulIngredients = document.querySelector('#ingredients');
+
+const fragment = document.createDocumentFragment();
+
+ingredients.forEach((ingredient) => {
+  const li = document.createElement('li');
+  
+  li.textContent = ingredient;
+  li.classList.add('item');
+    fragment.appendChild(li);
+});
+ulIngredients.appendChild(fragment);
